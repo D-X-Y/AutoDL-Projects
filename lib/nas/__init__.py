5eed8101a78d223a20a43494176051298b24ac3a
D-X-Y <280835372@qq.com>
2019-05-28 13:22:37 +0800

    delete confusing files

--- lib/nas/__init__.py
+++ lib/nas/__init__.py
@@ -1,7 +1,6 @@
 ##################################################
 # Copyright (c) Xuanyi Dong [GitHub D-X-Y], 2019 #
 ##################################################
-from .model_search    import Network
 from .CifarNet        import NetworkCIFAR
 from .ImageNet        import NetworkImageNet
 
