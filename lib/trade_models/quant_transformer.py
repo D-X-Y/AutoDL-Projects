756218974f0f24558d046290178fdbc43083af8b
D-X-Y <280835372@qq.com>
2021-03-30 12:25:47 +0000

    Fix bugs in .to(cpu)

--- lib/trade_models/quant_transformer.py
+++ lib/trade_models/quant_transformer.py
@@ -143,6 +143,19 @@ class QuantTransformer(Model):
             device = "cpu"
         self.device = device
         self.model.to(self.device)
+        # move the optimizer
+        for param in self.train_optimizer.state.values():
+            # Not sure there are any global tensors in the state dict
+            if isinstance(param, torch.Tensor):
+                param.data = param.data.to(device)
+                if param._grad is not None:
+                    param._grad.data = param._grad.data.to(device)
+            elif isinstance(param, dict):
+                for subparam in param.values():
+                    if isinstance(subparam, torch.Tensor):
+                        subparam.data = subparam.data.to(device)
+                        if subparam._grad is not None:
+                            subparam._grad.data = subparam._grad.data.to(device)
 
     def loss_fn(self, pred, label):
         mask = ~torch.isnan(label)
