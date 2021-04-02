0e35d8b15669105f0b941a9314be4a60d4116e52
D-X-Y <280835372@qq.com>
2021-04-01 20:36:41 -0700

    Upgrade default bashrc

--- .latent-data/init-configs/setup.sh
+++ .latent-data/init-configs/setup.sh
@@ -6,6 +6,8 @@ echo "script-directory: $script_dir"
 
 cp ${script_dir}/tmux.conf ~/.tmux.conf
 cp ${script_dir}/vimrc ~/.vimrc
+cp ${script_dir}/bashrc ~/.bashrc
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-4.7.12.1-Linux-x86_64.sh
+wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
 
